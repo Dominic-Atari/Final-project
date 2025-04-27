5828318cda1c3244cae57bfa7223b08128e62f92
Dominic-Atari <kikosimikehop2@gmail.com>
2025-04-27 05:16:43 -0500

    Delete company_finalProject directory

--- company_finalProject/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO department (department) VALUES ('atari');
-INSERT INTO department (department) VALUES ('Torit');
-INSERT INTO department (department) VALUES ('Juba');
-INSERT INTO department (department) VALUES ('Lincoln NE');
